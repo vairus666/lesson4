2292fe90decb4e5f69236dce94bcfe53a4c752dc
vairus666 <43839255+vairus666@users.noreply.github.com>
2021-03-11 06:30:58 +0300

    Create same_lists.py

---
+++ same_lists.py
@@ -0,0 +1,9 @@
+import collections
+
+l1 = ['asd', 20, 30, 40, 50]
+l2 = [ 30, 20, 'asd', 50, 40]
+
+if collections.Counter(l1) == collections.Counter(l2):
+	print ("Lists l1 and l2 same")
+else:
+	print ("Lists l1 and l2 not same")
